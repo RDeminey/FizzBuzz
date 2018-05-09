fee0c6440088e9261fc4e3161adedd78789e114a
RDeminey <38655139+RDeminey@users.noreply.github.com>
2018-05-09 08:34:51 +0200

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbersList = Enumerable.Range(1, 100).ToList();
+            
+                foreach (int element in numbersList)
+                {
+                if ((element % 3 == 0) && (element % 5 == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                if (element % 3 == 0)
+                {
+                     Console.WriteLine("Fizz");
+                }
+                 else if (element % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else
+                {
+                    Console.WriteLine(element);
+                }
+                     
+                             
+               
+                }
+        }
+    }
+}
